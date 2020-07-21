4bea27a6fb62207e4965e6202012153dc957292c
fehiora <mafe.hior@gmail.com>
2020-07-21 18:59:17 -0500

    Añadiendo if

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-let peso = prompt("Ingresa tu peso: ");
-let altura = prompt("Ingreas tu altura en metros, separando los centímetros con un punto: ");
+// let peso = prompt("Ingresa tu peso: ");
+// let altura = prompt("Ingreas tu altura en metros, separando los centímetros con un punto: ");
 
-function IMC (peso, altura){
+function IMC (){
+
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
     let resultado = peso/(altura^2);
+ 
+    if (resultado <=15){
+        document.write("Su IMC es: " + resultado + " Se encuentra en delgadez muy severa");
+    }else if (resultado >15 && resultado <= 15.9){
+        document.write("Su IMC es: " + resultado + " Se encuentra en delgadez severa");
+    }else if (resultado >=16 && resultado <= 18.4){
+        document.write("Su IMC es: " + resultado + " Se encuentra delgad@");
+    }else if (resultado >=18.5 && resultado <= 24.9){
+        document.write("Su IMC es: " + resultado + " Su peso es saludable");
+    }else if (resultado >=25 && resultado <= 29.9){
+        document.write("Su IMC es: " + resultado + " Se encuentra en sobrepeso");
+    }else if (resultado >=30 && resultado <= 34.9){
+        document.write("Su IMC es: " + resultado + " Se encuentra en obesidad severa");
+    }else if (resultado >= 40){
+        document.write("Su IMC es: " + resultado + " Se encuentra en obesidad mórbida");
+    }
 
-     document.write("Tu índice de masa corporal es: " + resultado);
 }
 
-IMC(peso,altura);
+/*function calcular(){
+    
+    let rsp = document.getElementById("respuesta");
+    rsp.textContent = IMC(peso, altura);
+}*/
